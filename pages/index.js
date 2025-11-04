91efc6371252a0476355c6aaea6852d1583ad3df
Julio Lemos <60183908+lemos-julio@users.noreply.github.com>
2025-11-04 19:33:57 +0000

    Adicionado uma mudança no texto

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const date = new Date()
 const getDate =  date.toLocaleDateString('pt-BR')
 
 function Home(){
-    return <h1> Site Rodando na vercel {getDate}</h1>
+    return <h1> Site Rodando na vercel online, na data: {getDate}</h1>
 }
 
 export default Home;
