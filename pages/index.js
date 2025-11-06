6d0a61ad157796644ab3ee8b41e9a8c4531b5f83
Julio Lemos <60183908+lemos-julio@users.noreply.github.com>
2025-11-06 18:49:07 +0000

    Adicionado  e

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-const date = new Date()
-const getDate =  date.toLocaleDateString('pt-BR')
+const date = new Date();
+const getDate = date.toLocaleDateString("pt-BR");
 
-function Home(){
-    return <h1> Site Rodando na vercel online, na data: {getDate}</h1>
+function Home() {
+  return <h1> Site Rodando na vercel online, na data: {getDate}</h1>;
 }
 
 export default Home;
